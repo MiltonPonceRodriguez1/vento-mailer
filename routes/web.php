d5b13f0cc70d32a74b9ead44b4fc35324a788e35
MiltonPonceRodriguez1 <miltonponceipn@gmail.com>
2022-09-24 16:04:12 -0500

    masive-python-php

--- routes/web.php
+++ routes/web.php
@@ -41,28 +41,43 @@ Route::get('/send/{table?}', function ($table = "developers") {
 
     $data_email['reccomend_3_hrf'] = "https://vento.com/tornado-250/";
 
+    $data = array(
+        'status' => 400,
+        'msg' => 'Error'
+    );
+
     try {
         $users = DB::table($table)->get();
     } catch (\Throwable $th) {
-        echo "<center><h1 style='color: red;'>Error La Tabla No Existe</h1></center>";
+        echo json_encode($data);
+        // echo "<center><h1 style='color: red;'>Error La Tabla No Existe</h1></center>";
         die();
     }
 
-    if(count($users) < 15) {
-        for($i=0; $i < count($users); $i++) {
-            echo "<h3>".$users[$i]->NOMBRE." => ".$users[$i]->TOKEN."</h3>";
-        }
-    }
+    // if(count($users) < 15) {
+    //     for($i=0; $i < count($users); $i++) {
+    //         echo "<h3>".$users[$i]->NOMBRE." => ".$users[$i]->TOKEN."</h3>";
+    //     }
+    // }
 
-    for($i=0; $i < count($users); $i++) {
-        $data_email['TOKEN'] = $users[$i]->TOKEN;
-        Mail::send('mails.mail', $data_email, function($message) use ($users, $i) {
-            //$message->to($users[$i]->EMAIL)->subject('Hello '.$users[$i]->NOMBRE.', pruebita xD.');
-            $message->to($users[$i]->EMAIL)->subject('¡Da el Grito con Motos Vento!');
-        });
-    }
+    // for($i=0; $i < count($users); $i++) {
+    //     $data_email['TOKEN'] = $users[$i]->TOKEN;
+    //     Mail::send('mails.mail', $data_email, function($message) use ($users, $i) {
+    //         //$message->to($users[$i]->EMAIL)->subject('Hello '.$users[$i]->NOMBRE.', pruebita xD.');
+    //         $message->to($users[$i]->EMAIL)->subject('¡Da el Grito con Motos Vento!');
+    //     });
+    // }
 
-    return "<h1 style='text-align: center;'><span style='color: rgb(35, 111, 161); font-family: 'comic sans ms', sans-serif;'>CORREO ENVIADO CORRECTAMENTE</span></h1>";  
+    
+    $data = array(
+        'status' => 200,
+        'msg' => 'Success',
+        'users' => $users
+    );
+    
+    echo json_encode($data);
+
+    // return "<h1 style='text-align: center;'><span style='color: rgb(35, 111, 161); font-family: 'comic sans ms', sans-serif;'>CORREO ENVIADO CORRECTAMENTE</span></h1>";  
 
 });
 
