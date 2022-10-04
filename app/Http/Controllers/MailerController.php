8b586ec0a2dc2800c5d5af6e6952bb7ee9491601
MiltonPonceRodriguez1 <miltonponceipn@gmail.com>
2022-10-03 21:50:43 -0500

    Tandeo & Unsubscribe Working

--- app/Http/Controllers/MailerController.php
+++ app/Http/Controllers/MailerController.php
@@ -9,9 +9,13 @@ use Illuminate\Support\Facades\Mail;
 class MailerController extends Controller
 {
     public function send(Request $request) {
+        # INDICE DE INICIO DEL ENVIO
         $index_init = $request->input('index_init');
+
+        # INDICE DE FINAL DEL ENVIO
         $index_end = $request->input('index_end');
 
+        # INICIO DE LOS DATOS DEL ENVIO
         $data_email['date'] = "27sep22";
 
         $data_email['content_href'] = "https://www.vento.com/ventodays/?utm_source=mailing&utm_medium=email&utm_campaign=NuevoMailing_VentoDays_30Sep2022";
@@ -32,23 +36,25 @@ class MailerController extends Controller
         $data_email['reccomend_2_hrf'] = "https://www.vento.com/rocketman-sport-250/?utm_source=mailing&utm_medium=email&utm_campaign=NuevoMailing_VRacer250_27Sep2022";
 
         $data_email['reccomend_3_hrf'] = "https://www.vento.com/rocketman-racing-250/?utm_source=mailing&utm_medium=email&utm_campaign=NuevoMailing_VRacer250_27Sep2022";
+        # FIN DE LOS DATOS DEL ENVIO
 
         try {
             $users = DB::table($request->input('table'))->get();
             
-            // for($i=$index_init; $i <= $index_end; $i++) {
-            //     $data_email['TOKEN'] = $users[$i]->TOKEN;
-            //     Mail::send('mails.mail', $data_email, function($message) use ($users, $i) {
-            //         $message->to($users[$i]->EMAIL)->subject('Hola '.$users[$i]->NOMBRE.', DESDE PYTHON VERSION II');
-            //     });
-            // }
+            # ENVIO DE MAILS MASIVOS
+            for($i=$index_init; $i < $index_end; $i++) {
+                $data_email['TOKEN'] = $users[$i]->TOKEN;
+                Mail::send('mails.mail', $data_email, function($message) use ($users, $i) {
+                    $message->to($users[$i]->EMAIL)->subject('Hola '.$users[$i]->NOMBRE.', DESDE PYTHON VERSION IV');
+                });
+            }
 
             $data = array(
                 'code' => 200,
                 'status' => 'success',
-                'msg' => "Todo Chido xD!",
+                'msg' => "Tandeo Enviado Correctamente!",
                 'init' => $users[$index_init]->NOMBRE,
-                'end' => $users[$index_end]->NOMBRE
+                'end' => $users[$index_end - 1]->NOMBRE
             );
         } catch (\Throwable $th) {
             $data = array(
@@ -62,7 +68,6 @@ class MailerController extends Controller
     }
 
     public function getTotalUsers(Request $request) {
-    
         try {
             $users = DB::table($request->input('table'))->get();
             
@@ -82,5 +87,34 @@ class MailerController extends Controller
 
         return response()->json($data, $data['code']);
         
+    }
+
+    public function unsubscribe($token) {
+        $user = DB::table('users')->where('TOKEN', $token)->first();
+        
+        if (is_object($user)) {
+            $test = DB::insert('INSERT INTO unsubscribe_users (ID, NOMBRE, EMAIL) values (?, ?, ?)', [null, $user->NOMBRE, $user->EMAIL]);
+            $response = DB::table('users')->where('TOKEN', $token)->delete();
+    
+            $data = array(
+                'code' => 200,
+                'status' => 'success',
+                'title' => 'Suscripción Cancelada Correctamente!',
+                'subtitle' => 'Esperamos Verte Pronto ...',
+                'color' => '#208637'
+            );
+        } else {
+            $data = array(
+                'code' => 400,
+                'status' => 'error',
+                'title' => 'Error al Cancelar la Suscripción!',
+                'subtitle' => 'Usuario no localizado.',
+                'color' => '#A51D2A'
+            );
+        }
+    
+        return view('unsubscribe', array(
+            'data' => $data
+        ));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,82 +19,27 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/send/{table?}', function ($table = "developers") {
+Route::get('/unsubscribe/{token}', function($token){
+    $user = DB::table('users')->where('TOKEN', $token)->first();
+        
+    if (is_object($user)) {
+        $test = DB::insert('INSERT INTO unsubscribe_users (ID, NOMBRE, EMAIL) values (?, ?, ?)', [null, $user->NOMBRE, $user->EMAIL]);
+        $response = DB::table('users')->where('TOKEN', $token)->delete();
 
-    $data_email['date'] = "30sep22";
-
-    $data_email['content_href'] = "https://www.vento.com/ventodays/?utm_source=mailing&utm_medium=email&utm_campaign=NuevoMailing_VentoDays_30Sep2022";
-
-    $data_email['moto_model'] = "V-RACER 250";
-    $data_email['discount'] = "$16,000";
-
-    $data_email['planes'] = "https://www.vento.com/planes-vracer-250/?utm_source=mailing&utm_medium=email&utm_campaign=NuevoMailing_VRacer250_27Sep2022";
-
-    $data_email['cilindrada'] = "250 c.c.";
-    $data_email['vel_max'] = "130 km/h";
-    $data_email['rendimiento'] = "30 km/l";
-    $data_email['velocidades'] = "5";
-    $data_email['potencia'] = "18 HP";
-
-    $data_email['reccomend_1_hrf'] = "https://www.vento.com/rocketman-carrera-250/?utm_source=mailing&utm_medium=email&utm_campaign=NuevoMailing_VRacer250_27Sep2022";
-
-    $data_email['reccomend_2_hrf'] = "https://www.vento.com/rocketman-sport-250/?utm_source=mailing&utm_medium=email&utm_campaign=NuevoMailing_VRacer250_27Sep2022";
-
-    $data_email['reccomend_3_hrf'] = "https://www.vento.com/rocketman-racing-250/?utm_source=mailing&utm_medium=email&utm_campaign=NuevoMailing_VRacer250_27Sep2022";
-
-    $data = array(
-        'code' => 400,
-        'msg' => 'Error',
-        'table' => $table,
-        'count' => 0
-    );
-
-    try {
-        $users = DB::table($table)->get();
-    } catch (\Throwable $th) {
-        echo json_encode($data);
-        die();
-    }
-
-    $n = count($users);
-
-    // for($i=0; $i < $n; $i++) {
-    //     $data_email['TOKEN'] = $users[$i]->TOKEN;
-    //     Mail::send('mails.mail', $data_email, function($message) use ($users, $i) {
-    //         $message->to($users[$i]->EMAIL)->subject('Hola '.$users[$i]->NOMBRE.', ¿Irás a la rodada de Vento Days? Sigue estos consejos');
-    //     });
-    // }
-
-    
-    $data = array(
-        'code' => 200,
-        'msg' => 'Success',
-        'users' => $users,
-        'table' => $table,
-        'count' => $n
-    );
-    
-    echo json_encode($data);
-});
-
-Route::get('/unsubscribe/{TOKEN}', function($TOKEN){
-    $data = array(
-        'code' => 400,
-        'status' => 'error',
-        'title' => 'Error al Cancelar la Suscripción!',
-        'subtitle' => 'Usuario no localizado.',
-        'color' => '#A51D2A'
-    );
-
-    $response = DB::table('developers')->where('TOKEN', $TOKEN)->delete();
-
-    if($response > 0) {
         $data = array(
             'code' => 200,
             'status' => 'success',
             'title' => 'Suscripción Cancelada Correctamente!',
             'subtitle' => 'Esperamos Verte Pronto ...',
             'color' => '#208637'
+        );
+    } else {
+        $data = array(
+            'code' => 400,
+            'status' => 'error',
+            'title' => 'Error al Cancelar la Suscripción!',
+            'subtitle' => 'Usuario no localizado.',
+            'color' => '#A51D2A'
         );
     }
 
@@ -103,14 +48,6 @@ Route::get('/unsubscribe/{TOKEN}', function($TOKEN){
     ));
 });
 
-Route::get('/pruebas/{name?}', function($name = null){
-    $text = '<h2>Texto desde una ruta !!</h2>';
-    $text .= 'Nombre: '.$name;
-
-
-    
-    echo $text;
-});
-
 Route::post('/api/count', [MailerController::class, 'getTotalUsers']);
 Route::post('/api/send', [MailerController::class, 'send']);
+Route::get('/api/unsubscribe/{token}', [MailerController::class, 'unsubscribe']);
